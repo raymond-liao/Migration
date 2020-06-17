53627f041faa564212ad5471062c9889d2bef99e
raniys.liao <chengjia_liao@hotmail.com>
2020-06-17 11:48:07 +0800

    improve UI

--- Migration/EmojiMemoryGameView.swift
+++ Migration/EmojiMemoryGameView.swift
@@ -21,7 +21,6 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         .foregroundColor(.orange)
-        .font(.largeTitle)
     }
 }
 
@@ -29,18 +28,33 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
         }
     }
+    
+    func body(for size: CGSize) -> some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10
+    let edgeLineWidth: CGFloat = 3
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
+    
 }
-
-
 
 
 struct ContentView_Previews: PreviewProvider {
