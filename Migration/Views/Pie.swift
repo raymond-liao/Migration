3e50d9a0943b9f014c37941d2c35caf0a491fe14
raniys.liao <chengjia_liao@hotmail.com>
2020-06-18 14:15:47 +0800

    draw a Pie for emojis

---
+++ Migration/Views/Pie.swift
@@ -0,0 +1,39 @@
+//
+//  Pie.swift
+//  Migration
+//
+//  Created by raniys on 6/18/20.
+//  Copyright © 2020 raniys. All rights reserved.
+//
+
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool = false
+
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height) / 2
+        let start = CGPoint(
+            x: center.x + radius * cos(CGFloat(startAngle.radians)),
+            y: center.y + radius * sin(CGFloat(startAngle.radians))
+        )
+
+        var path = Path()
+        path.move(to: center)
+        path.addLine(to: start)
+        path.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: clockwise
+        )
+        path.addLine(to: center)
+
+        return path
+    }
+
+}
